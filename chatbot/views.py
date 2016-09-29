935fde0ef27946b63f00b1c0ae755c6c241a6c4d
vishrutkohli <kohlivishrut@gmail.com>
2016-09-30 01:39:07 +0530

    checking

--- chatbot/views.py
+++ chatbot/views.py
@@ -80,7 +80,7 @@ class MyChatBotView(generic.View):
 				except Exception as e:
 					print e
 					pass
-	post_facebook_message(sender_id,'Is there anything more to help.?')				
+		post_facebook_message(sender_id,'Is there anything more to help.?')				
 		return HttpResponse()  
 
 def index(request):
