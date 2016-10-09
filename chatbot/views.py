781bb1bb60c5b526eab734ad470825f23f73fff2
vishrutkohli <kohlivishrut@gmail.com>
2016-10-09 10:20:37 +0530

    checking spreadsheets

--- chatbot/views.py
+++ chatbot/views.py
@@ -77,9 +77,10 @@ class MyChatBotView(generic.View):
 				try:
 					sender_id = message['sender']['id']
 					message_text = message['message']['text']
-					for i in range(len(a)):
+					print "hahahshsh",len(a)
+					# for i in range(len(a)):
 
-						#post_facebook_message(sender_id,a[i])
+					# 	post_facebook_message(sender_id,a[i])
 
 					
 					
