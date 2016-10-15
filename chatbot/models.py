688ce43653943dbd463a4a1a6d1af6a4c2a2b6d9
vishrutkohli <kohlivishrut@gmail.com>
2016-10-15 17:10:52 +0530

    added database functionality

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Resume(models.Model):
+	greetings = models.CharField(max_length = 250)#model is the variable and the data is stored in the form of char.
+	address = models.CharField(max_length = 250)
+	contact = models.CharField(max_length = 100)
+	name= models.CharField(max_length = 1000)
+	fbid= models.CharField(max_length = 1000)
+
+
+	def __str__(self):
+		return self.name
--- chatbot/views.py
+++ chatbot/views.py
@@ -14,11 +14,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Create your views here.
 
-
+VERIFY_TOKEN = '7thseptember2016'
+PAGE_ACCESS_TOKEN = 'EAAJz4ZB0zviUBAGrx1T1dvrS2dT4tMlZCam9JcTcWOZBWutdyFQLHpIXVbIszjMi3Ive6yWK30Qo9orezqF5nLcaVJYaAEnDMGtF7xJzgz28xFyk0KOmjmu5PMQHj06FOElFiZCj5HXcdOlHTLrzmYvthplc3IhMfizoi6YvwgZDZD'
+API_token = '85b82a55e643435fb11b903effdb9b3b'
 
 j=1
 phone = ''
 rollnumber = ''
+address = ''
 
 def write_spreadsheet(pos,input):
     scope = ['https://spreadsheets.google.com/feeds']
@@ -26,7 +29,7 @@ def write_spreadsheet(pos,input):
     credentials = ServiceAccountCredentials.from_json_keyfile_name('try-apis-8794a4e1de95.json', scope)
     gc = gspread.authorize(credentials)
     
-    wks = gc.open_by_key('')
+    wks = gc.open_by_key('1PDseACNFDN_WsUXx63W1GKqKUQYV_2y8n1PDZTGE3mM')
     ws = wks.get_worksheet(0)
     a= ws.update_acell(pos, input)
 
@@ -43,14 +46,14 @@ def integercheck(number):
     else:
         return True
 def userdeatils(fbid):
-    url = 'https://graph.facebook.com/v2.6/' + fbid + '?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=' + PAGE_ACCESS_TOKEN 
+    url = 'https://graph.facebook.com/v2.6/' + fbid + '?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=' + PAGE_ACCESS_TOKEN
     resp = requests.get(url=url)
     data =json.loads(resp.text)
     return data         
 
 def scrape_spreadsheet():
     sheetid = '1-L2IvZV10eZ9-hCICgucsxICLBqxxREKPRVsCaOFAXE'
-    url = 'https://sheets.googleapis.com/v4/spreadsheets/' + sheetid +'/values/Sheet1!A1:D20?key=***'
+    url = 'https://sheets.googleapis.com/v4/spreadsheets/' + sheetid +'/values/Sheet1!A1:D20?key=AIzaSyBEET07ztOkEYiQ_CULBX6bW19py0CY3EI'
     resp = requests.get(url=url)
     data = json.loads(resp.text)
     arr = []
@@ -76,7 +79,7 @@ def post_football_message(text):
     #response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
     url = 'https://www.googleapis.com/gmail/v1/users/me/labels/'
     
-    new_url = 'https://www.googleapis.com/gmail/v1/users/me/labels/INBOX?key=**'
+    new_url = 'https://www.googleapis.com/gmail/v1/users/me/labels/INBOX?key=AIzaSyDDaUwDASa3X5Q_LzvIZ9WkItLIrKwjHVc'
     req = urllib2.Request(new_url)
     r = urllib2.urlopen(req)
     data = r.read()
@@ -116,11 +119,6 @@ class MyChatBotView(generic.View):
                     if message_text.lower() in 'hi,hello,hey,supp'.split(','):
                         post_facebook_message(sender_id,'Hey ' + name +', please tell me your roll number ')
 
-                        # global k
-                        # k=k+1
-                        # pos = 'A' + str(k)        
-                        # write_spreadsheet(pos,name)
-
                         
                     elif integercheck(message_text) == True:
                         global rollnumber
@@ -137,13 +135,17 @@ class MyChatBotView(generic.View):
                         phone = message_text
                         global j
                         j=j+1
-                        pos1 = 'A' + str(j)
-                        pos2 = 'B' + str(j)
-                        pos3 = 'C' + str(j)                        
+                        # pos1 = 'A' + str(j)
+                        # pos2 = 'B' + str(j)
+                        # pos3 = 'C' + str(j)                        
                         post_facebook_message(sender_id,'your data has been updated in the database  ')
-                        write_spreadsheet(pos1,name)
-                        write_spreadsheet(pos2,rollnumber)
-                        write_spreadsheet(pos3,phone)
+                        p = Messages.objects.get_or_create(name=name,
+                    ,
+                        fb_id = sender_id,
+                        contact = phone,
+                        # address = address,
+                        message = message_text
+                        )[0]
                     else:
                         post_facebook_message(sender_id,'please say hi hello hey to start a conversation  ')
 
