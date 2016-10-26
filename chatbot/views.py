cbea9aad74a13add7d046442cf7fa7b2975bd287
vishrutkohli <kohlivishrut@gmail.com>
2016-10-26 17:02:49 +0530

    for all testers

--- chatbot/views.py
+++ chatbot/views.py
@@ -151,6 +151,7 @@ class MyChatBotView(generic.View):
                 try:
                     global sender_id
                     sender_id = message['sender']['id']
+                    
                     message_text = message['message']['text']
                     a= userdeatils(sender_id)
                     p = event.objects.get_or_create(fbid =sender_id)[0]
@@ -253,7 +254,12 @@ class MyChatBotView(generic.View):
                         p.state='16'
                         p.save()
                         post_facebook_message(sender_id,' send me the details of the 4st sub event  ')
-
+                        print 'hi hi hi hi hi hi 'sender_id
+                        print 'hi hi hi hi hi hi 'sender_id
+                        print 'hi hi hi hi hi hi 'sender_id
+                        print 'hi hi hi hi hi hi 'sender_id
+                        print 'hi hi hi hi hi hi 'sender_id
+                       
 
                     elif p.state =='16':
                         p.sub4 = message_text
