f1bdbfc3dd229e7476f6689596d559e39c5089ea
vishrutkohli <kohlivishrut@gmail.com>
2016-10-28 23:52:16 +0530

    checking card

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from chatbot.models import event
+from chatbot.models import event,resume_input
 
 # Register your models here.
 
 admin.site.register(event)
+admin.site.register(resume_input)
 
