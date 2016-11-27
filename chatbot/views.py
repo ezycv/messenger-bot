70739c39052fd4beff6b239068a2d44f14066b98
vishrutkohli <kohlivishrut@gmail.com>
2016-11-28 01:13:51 +0530

    trying picture dillemma

--- chatbot/views.py
+++ chatbot/views.py
@@ -464,12 +464,12 @@ class MyChatBotView(generic.View):
 
     def post(self, request, *args, **kwargs):
         incoming_message= json.loads(self.request.body.decode('utf-8'))
-        print  incoming_message
+        print  "bibibibibi" + incoming_message
         
 
         for entry in incoming_message['entry']:
             for message in entry['messaging']:
-                print message
+                # print message
 
                 try:
                     global sender_id
