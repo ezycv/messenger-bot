0582016606d596a13208e243a9e70bb972704406
vishrutkohli <kohlivishrut@gmail.com>
2017-03-05 18:37:10 +0530

    aa

--- hellobot/settings.py
+++ hellobot/settings.py
@@ -73,16 +73,20 @@ STATICFILES_DIRS = (
 )
 
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myproject',
-        'USER': 'root',
-        'PASSWORD': 'awesome',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
